28f6c80f11d77e9659e284ac4375026e0f29b3e8
Jimmy Chen <ccking37@gmail.com>
2023-02-15 22:49:08 +0800

    Modify the code after watching the solution video for exercise 3

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { range } from '../../utils';
 
-function Guess({ guess }) {
+function Guess({ value }) {
 	return (
 		<p className="guess">
-			{range(5).map(index => (
-				<span key={index} className="cell">
-					{guess && guess[index]}
+			{range(5).map(num => (
+				<span key={num} className="cell">
+					{value ? value[num] : undefined}
 				</span>
 			))}
 		</p>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -6,8 +6,8 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 function GuessResults({ guesses }) {
 	return (
 		<div className="guess-results">
-			{range(NUM_OF_GUESSES_ALLOWED).map(index => (
-				<Guess key={index} guess={guesses[index]} />
+			{range(NUM_OF_GUESSES_ALLOWED).map(num => (
+				<Guess key={num} value={guesses[num]} />
 			))}
 		</div>
 	);
