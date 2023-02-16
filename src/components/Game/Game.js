ad7744fce1fa6644c07bad2840485e25bf655234
Jimmy Chen <ccking37@gmail.com>
2023-02-16 14:45:53 +0800

    Finish exercise 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,6 @@ import React from 'react';
 import GuessResults from '../GuessResults';
 import GuessInput from '../GuessInput';
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
-
 function Game() {
 	const [guesses, setGuesses] = React.useState([]);
 
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { range } from '../../utils';
 
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+import { checkGuess } from '../../game-helpers';
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
 function Guess({ value }) {
+	const result = checkGuess(value, answer);
 	return (
 		<p className="guess">
 			{range(5).map(num => (
-				<span key={num} className="cell">
-					{value ? value[num] : undefined}
+				<span key={num} className={`cell ${result && result[num].status}`}>
+					{result ? result[num].letter : undefined}
 				</span>
 			))}
 		</p>
